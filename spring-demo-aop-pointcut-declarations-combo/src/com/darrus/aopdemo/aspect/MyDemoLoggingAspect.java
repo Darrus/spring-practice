cfd33e2ddb5e6cf906eb13e003efdf791bd3e7f0
Darrus <darrusgoh@gmail.com>
2020-03-27 21:42:55 +0800

    38:334 - Pointcut Declarations, Combining, and Logic Gates

---
+++ spring-demo-aop-pointcut-declarations-combo/src/com/darrus/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,35 @@
+package com.darrus.aopdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class MyDemoLoggingAspect
+{
+	@Pointcut("execution(* com.darrus.aopdemo.dao.*.*(..))")
+	private void forDaoPackage(){}
+	
+	@Pointcut("execution(* com.darrus.aopdemo.dao.*.get*(..))")
+	private void getter() {}
+	
+	@Pointcut("execution(* com.darrus.aopdemo.dao.*.set*(..))")
+	private void setter() {}
+	
+	@Pointcut("forDaoPackage() && !(getter() || setter())")
+	private void forDaoPackageNoGetterSetter() {}
+	
+	@Before("forDaoPackageNoGetterSetter()")
+	public void beforeAddAccountAdvice()
+	{
+		System.out.println("\n====>>> Executing @Before advice on method");
+	}
+	
+	@Before("forDaoPackageNoGetterSetter()")
+	public void performApiAnalytics()
+	{
+		System.out.println("\n====>>> Performing API analytics");
+	}
+}
