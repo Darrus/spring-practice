b242b098313b01d1e760e7e89dddba94f423a4c1
Darrus <darrusgoh@gmail.com>
2020-03-28 12:26:02 +0800

    41:344 - AOP @AfterReturning advice

---
+++ spring-demo-aop-after-returning/src/com/darrus/aopdemo/dao/AccountDAO.java
@@ -0,0 +1,66 @@
+package com.darrus.aopdemo.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.darrus.aopdemo.Account;
+
+@Component
+public class AccountDAO
+{
+	private String name;
+	private String serviceCode;
+	
+	public List<Account> findAccounts()
+	{
+		List<Account> accounts = new ArrayList<Account>();
+		
+		Account temp1 = new Account("John", "Silver");
+		Account temp2 = new Account("Madhu", "Platinum");
+		Account temp3 = new Account("Luca", "Gold");
+		
+		accounts.add(temp1);
+		accounts.add(temp2);
+		accounts.add(temp3);
+		
+		return accounts;
+	}
+	
+	public void addAccount(Account account, Boolean vipFlag)
+	{
+		System.out.println(getClass() + ": DOING MY DB WORK: ADDING ACCOUNT");
+	}
+	
+	public boolean doWork()
+	{
+		System.out.println(getClass() + ": doWork()");
+		return false;
+	}
+
+	public String getName()
+	{
+		System.out.println(getClass() + ": in getName()");
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		System.out.println(getClass() + ": in setName()");
+		this.name = name;
+	}
+
+	public String getServiceCode()
+	{
+		System.out.println(getClass() + ": in getServiceCode()");
+		return serviceCode;
+	}
+
+	public void setServiceCode(String serviceCode)
+	{
+		System.out.println(getClass() + ": in setServiceCode()");
+		this.serviceCode = serviceCode;
+	}
+	
+}
