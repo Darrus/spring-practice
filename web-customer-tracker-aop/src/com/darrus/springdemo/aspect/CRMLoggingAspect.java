49f51836cc1bfa2b49c9ac89457150b046209833
Darrus <darrusgoh@gmail.com>
2020-03-30 01:04:36 +0800

    45:359 - Spring AOP & MVC App

---
+++ web-customer-tracker-aop/src/com/darrus/springdemo/aspect/CRMLoggingAspect.java
@@ -0,0 +1,53 @@
+package com.darrus.springdemo.aspect;
+
+import java.util.logging.Logger;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+
+@Aspect
+@Component
+public class CRMLoggingAspect
+{
+	private Logger logger = Logger.getLogger(CRMLoggingAspect.class.getName());
+	
+	@Pointcut("execution(* com.darrus.springdemo.controller.*.*(..))")
+	private void forControllerPackage() {}
+	
+	@Pointcut("execution(* com.darrus.springdemo.service.*.*(..))")
+	private void forServicePackage() {}
+	
+	@Pointcut("execution(* com.darrus.springdemo.dao.*.*(..))")
+	private void forDaoPackage() {}
+	
+	@Pointcut("forControllerPackage() || forServicePackage() || forDaoPackage()")
+	private void forAppFlow() {}
+	
+	@Before("forAppFlow()")
+	private void before(JoinPoint joinPoint)
+	{
+		String method = joinPoint.getSignature().toShortString();
+		logger.info("=====>> in @Before: calling method: " + method);
+		
+		Object[] args = joinPoint.getArgs();
+		
+		for(Object arg : args)
+		{
+			logger.info("=====>> argument: " + arg);
+		}
+	}
+	
+	@AfterReturning(pointcut="forAppFlow()",
+			returning="result")
+	private void afterReturning(JoinPoint joinPoint, Object result)
+	{
+		String method = joinPoint.getSignature().toShortString();
+		logger.info("=====>> in @AfterReturning: from method: " + method);
+		logger.info("=====>> result: " + result);
+	}
+}
